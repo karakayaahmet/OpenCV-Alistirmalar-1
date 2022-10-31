a918b8624709a5e01fbef79f127aa4e04f386b91
Ahmet Karakaya <karakayahmet00@gmail.com>
2022-10-31 13:36:05 +0300

    hsv ile nesne saptama 2 tamamlandı.

---
+++ hsv-nesne-saptama2.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+cap = cv2.VideoCapture(0)
+cv2.namedWindow("Trackbar")
+
+cv2.createTrackbar("lh", "Trackbar", 0, 179, nothing)
+cv2.createTrackbar("ls", "Trackbar", 0, 255, nothing)
+cv2.createTrackbar("lv", "Trackbar", 0, 255, nothing)
+cv2.createTrackbar("uh", "Trackbar", 0, 179, nothing)
+cv2.createTrackbar("us", "Trackbar", 0, 255, nothing)
+cv2.createTrackbar("uv", "Trackbar", 0, 255, nothing)
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lh = cv2.getTrackbarPos("lh", "Trackbar")
+    ls = cv2.getTrackbarPos("ls", "Trackbar")
+    lv = cv2.getTrackbarPos("lv", "Trackbar")
+    uh = cv2.getTrackbarPos("uh", "Trackbar")
+    us = cv2.getTrackbarPos("us", "Trackbar")
+    uv = cv2.getTrackbarPos("uv", "Trackbar")
+
+    lower_blue = np.array([lh, ls, lv])
+    upper_blue = np.array([uh, us, uv])
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    bitwise = cv2.bitwise_and(frame, frame, mask = mask)
+
+    cv2.imshow("frame", frame)
+    cv2.imshow("mask", mask)
+    cv2.imshow("bitwise", bitwise)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
